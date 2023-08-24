f7e46a9ee56ee00d8ba3a1f28af6fff547efb09b
sh4d0wy <sakshambhugra8@gmail.com>
2023-08-24 21:35:28 +0530

    Changes in name returned

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
   if(!req.file){
     return res.status(400).json({"error":"No file uploaded"});
   }
+
   res.json({
-    name:req.file.fieldname,
+    name:req.file.originalname,
     type:req.file.mimetype,
     size: req.file.size
   })
