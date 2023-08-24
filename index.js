691342434fcc8f523615b4e1b366fe83e8e5e798
sh4d0wy <sakshambhugra8@gmail.com>
2023-08-24 21:02:36 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
+
 var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  if(!req.file){
+    return res.status(400).json({"error":"No file uploaded"});
+  }
+  res.json({
+    name:req.file.fieldname,
+    type:req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
